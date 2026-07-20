0f5a0351c8480aa5c80555345671fa6d1c27ceff
Dan Bergren <bergren2@users.noreply.github.com>
2026-07-20 16:39:35 -0500

    run ruff

--- src/common/fibseq.py
+++ src/common/fibseq.py
@@ -1,17 +1,18 @@
 from collections.abc import Callable
 
+
 class FibSeq:
     def __init__(self, term0: int, term1: int):
         self.term0 = term0
         self.term1 = term1
 
-
     @staticmethod
     def constant_true(_):
-        return true
+        return True
 
-
-    def sum_of_terms(self, limit: int, condition: Callable[[int], bool] = constant_true):
+    def sum_of_terms(
+        self, limit: int, condition: Callable[[int], bool] = constant_true
+    ):
         num_sum = 0
         a = self.term0
         b = self.term1
--- tests/euler/test_problem3.py
+++ tests/euler/test_problem3.py
@@ -5,6 +5,7 @@ def test_sample():
     problem = Problem3()
     assert problem.solve(13195) == 29
 
+
 def test_actual():
     problem = Problem3()
     assert problem.solve(600851475143) == 6857
